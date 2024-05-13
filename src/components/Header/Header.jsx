43465f583ab1f5c669f2a230b754e8bb16b60d6d
Sanjayk46 <138973406+Sanjayk46@users.noreply.github.com>
2024-05-13 20:57:39 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
     <header className={classes.header}>
       <div className={classes.container}>
         <Link to="/" className={classes.logo}>
-          SK Restarunt
+          SK Restaurant
         </Link>
         <nav>
           <ul>
